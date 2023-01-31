816bfc1789fbf81cc9fc50af99b2a66ab447ea2f
Andrew Dupont <github@andrewdupont.net>
2023-01-31 09:39:32 -0800

    test: add unit test

--- spec/runner.js
+++ spec/runner.js
@@ -1,6 +1,19 @@
 'use babel';
 
 import { createRunner } from 'atom-jasmine3-test-runner';
+import childProcess from 'child_process';
+
+if (process.env.PATH === '/usr/bin:/bin:/usr/sbin:/sbin') {
+  // If the PATH value is the default, we're probably in a GUI spec-runner
+  // window that has failed to inherit its PATH variable from the window that
+  // spawned it. This happened sporadically in Atom but is happening
+  // consistently in Pulsar. This is a quick fix.
+  let shellOutput = childProcess.execFileSync(
+    process.env.SHELL,
+    ['-i', '-c', 'echo $PATH']
+  ).toString().trim().split('\n');
+  process.env.PATH = shellOutput[shellOutput.length - 1];
+}
 
 function setDefaultSettings(namespace, settings) {
   for (const name in settings) {
